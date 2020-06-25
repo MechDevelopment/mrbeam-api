7659123eef748f9c4bb8eec412bf625610ee2103
Di <divasilevski@gmail.com>
2020-06-25 21:41:17 +0300

    middleware - json header

--- server/server.ts
+++ server/server.ts
@@ -2,16 +2,24 @@ import express = require('express');
 
 const app: express.Application = express();
 
-app.get('/', function (req, res) : void {
-  res.send('Hello World!');
-});
+app.use(function(req, res, next) {
+  res.header("Content-Type",'application/json');
+  next();
+})
 
-app.get('/generate', function (req, res) : void {
-  res.send('generate');
-});
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({
+    GET: '/generate',
+    POST: '/generate /calculate'
+  }, null, 4));
+})
+
+app.get('/generate', function (req, res) {
+  res.send('in developing...');
+})
 
 const port: string = process.env.PORT || '3000';
 
-app.listen(port, function (): void {
+app.listen(port, function () {
   console.log('App is listening on port:', port);
-});
+})
