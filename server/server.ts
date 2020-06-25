272a06ed411eaf1a151f64404372b7072bca4a21
Di <divasilevski@gmail.com>
2020-06-25 20:24:05 +0300

    add process.env.PORT

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,17 @@
-import express = require('express')
+import express = require('express');
 
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) : void {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('App is listening on port 3000!');
+app.get('/generate', function (req, res) : void {
+  res.send('generate');
+});
+
+const port: string = process.env.PORT || '3000';
+
+app.listen(port, function (): void {
+  console.log('App is listening on port:', port);
 });
