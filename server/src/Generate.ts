e5c2a095a38c0936962b18a87094d7d27f1479ab
Di <divasilevski@gmail.com>
2020-06-30 15:20:24 +0300

    init structure

--- server/src/Generate.ts
+++ server/src/Generate.ts
@@ -11,3 +11,20 @@ interface Unit {
   value?: number|number[];
 }
 
+function initialSettings(): number[] {
+  const init: number[] = [];
+  return init;
+}
+
+function createUnits(count: number, length: number): Unit[] {
+  const units: Unit[] = [];
+  return units;
+}
+
+function Generate(level: Level = Level.Elementary, count: number = 0, length: number = 0) {
+  
+  initialSettings()
+
+  const units: Unit[] = createUnits(count, length);
+}
+
