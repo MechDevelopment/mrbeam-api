f27afd113ee0c62bcbd6cc23a5bd66fd4d4b93a9
Di <divasilevski@gmail.com>
2020-07-15 17:47:25 +0300

    addFixed

--- server/src/generate.ts
+++ server/src/generate.ts
@@ -5,16 +5,17 @@ import { randInt } from './services/algebra';
 
 const DEFAULT_LEVEL: GenerateLevel = 'elementary';
 
+
 function initSettings(gp: GenerateParameters): InitSettings {
   if (!gp.level) gp.level = DEFAULT_LEVEL;
 
   if (gp.level === 'random') {
-    const basicLevels: GenerateLevel[] = ['elementary', 'intermediate' , 'advanced'];
-    gp.level = basicLevels[randInt(0,2)]
+    const basicLevels: GenerateLevel[] = ['elementary', 'intermediate', 'advanced'];
+    gp.level = basicLevels[randInt(0, 2)]
   }
 
   if (!gp.unitsCount || gp.unitsCount < 2) {
-    gp.unitsCount = randInt(2, 5); /* else */ 
+    gp.unitsCount = randInt(2, 5); /* else */
     if (gp.level === 'intermediate') gp.unitsCount = randInt(3, 8);
     if (gp.level === 'advanced') gp.unitsCount = randInt(5, 10);
   }
@@ -27,28 +28,58 @@ function initSettings(gp: GenerateParameters): InitSettings {
 }
 
 
-function createUnits(count: number, length: number): Unit[] {
-  const units: Unit[] = []
+function createUnits(unitsCount: number, beamLength: number): Array<Unit> {
+  const units: Array<Unit> = []
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < unitsCount; i++) {
     units.push({
       id: nanoid(10),
       type: 'point',
-      x: length / (count - 1) * i
+      x: beamLength / (unitsCount - 1) * i
     })
   }
 
   return units;
 }
 
+
+function addFixed(units: Array<Unit>): void {
+  if (randInt(0, 1)) units[0].type = 'fixed';
+  else units[units.length - 1].type = 'fixed';
+}
+
+
+function addSimple(units: Array<Unit>): void {
+  
+}
+
+
+function addSupport(units: Array<Unit>): void {
+  if (units.length < 3) addFixed(units);
+
+  if (units.length > 2 && units.length < 6) {
+    if (randInt(0, 1)) addFixed(units);
+    else addSimple(units);
+  }
+
+  if (units.length > 5) addSimple(units);
+}
+
+
 export default function generate(gp: GenerateParameters = {}) {
 
   const { level, unitsCount, beamLength }: InitSettings = initSettings(gp);
+  const units: Array<Unit> = createUnits(unitsCount, beamLength);
+
+  addSupport(units);
 
 
-  const units: Unit[] = createUnits(unitsCount, beamLength);
 
-  console.log(level, unitsCount, beamLength, units );
+  console.log(level, unitsCount, beamLength, units);
+
+  if (level === 'elementary') {
+
+  }
 
 
 }
