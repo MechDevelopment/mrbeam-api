e0a552950852701208c6b548fc743d205d235549
Di <divasilevski@gmail.com>
2020-08-02 16:46:07 +0300

    handle exeptions (TypeErrors)

--- server/src/services/error.ts
+++ server/src/services/error.ts
@@ -5,18 +5,18 @@ export function handleErrors(units: Array<Unit>) {
   let countOfSimple = 0
 
   // Handle errors
-  if (!Array.isArray(units)) throw new Error('Bad data format, data should be an array')
-
-  if (units.length === 0) throw new Error('There are no units')
+  if (!Array.isArray(units)) throw new Error('TypeError | hint: data should be an array')
 
   for (let unit of units) {
-    if (!unit.hasOwnProperty('x')) throw new Error('Bad units format, units should have <x> property')
+    if (unit.toString() !== '[object Object]') throw new Error('TypeError | hint: units should be objects')
+    
+    if (!unit.hasOwnProperty('x')) throw new Error('TypeError | hint: units should have <x> property')
+    if (!unit.hasOwnProperty('type')) throw new Error('TypeError | hint: units should have <type> property')
 
-    if (typeof unit.x !== 'number' && !Array.isArray(unit.x)) {
-      throw new Error('Bad units <x>, should be number or Array<number>')
-    }
-
-    if (!unit.hasOwnProperty('type')) throw new Error('Bad units format, units should have <type> property')
+    
+    if (typeof unit.x !== 'number' && !Array.isArray(unit.x) && Object.is(+unit.x, NaN)) {
+      throw new Error('TypeError | hint: property <x> should be a number or an array')
+    }  
 
     if (!['force', 'moment', 'distload', 'material', 'fixed', 'simple', 'hinge'].includes(unit.type)) {
       throw new Error('Bad units <type>, should be force, moment, distload, material, fixed, simple, hinge')
@@ -30,7 +30,7 @@ export function handleErrors(units: Array<Unit>) {
     if (unit.type === 'simple') countOfSimple++
   }
 
-  if (units.length === 1) throw new Error('Not enough units')
+  if (units.length === 0 || units.length === 1) throw new Error('Not enough units')
 
   if (countOfFixed === 0 && countOfSimple < 2) {
     throw new Error('Bad units format, units should have minimum 2 simple or 1 fixed supports')
